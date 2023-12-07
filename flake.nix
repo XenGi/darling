e8888369f056851bbb8da40601743d2f7396ac4f
Ricardo Band <email@ricardo.band>
2023-12-07 09:39:34 +0100

    add flake

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "FTDI helper for the future. With FT230X, blinky lights and USB-C!";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            kicad
+            zip
+          ];
+        };
+      }
+    );
+}
+
